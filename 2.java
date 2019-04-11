ade2b512adf4a5d0a3bd5f6120a8a007a9e05da4
xuexi007 <44258352+xuexi007@users.noreply.github.com>
2019-04-11 15:57:06 +0800

    Update 2.java

--- 2.java
+++ 2.java
@@ -2,6 +2,10 @@ package com.example.springbootdemo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+*增加注释
+*
+*/
 
 @SpringBootApplication
 public class SpringbootDemo1Application {
